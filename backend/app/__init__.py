54a25d5bb6bf03019f2c426009d7a032db9d1c7b
Yashaswini Putta <yashaswiniputta09@gmail.com>
2025-05-28 19:24:13 -0500

    Added Watchlist endpoints and Supabase integration

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,9 +1,22 @@
 from flask import Flask
 from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+from dotenv import load_dotenv
+import os
+
+db = SQLAlchemy()
 
 def create_app():
+    load_dotenv()
+
     app = Flask(__name__)
-    CORS(app)  # Enable CORS for frontend-backend communication
+    CORS(app) #Enable CORS foe frontend-backend communication
+
+    # Load database URL from environment
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    db.init_app(app)
 
     from .routes.stocks import stocks_bp
     from .routes.watchlist import watchlist_bp
